68a20ee8178c7cb4b43487fc9e7c1a301fcea2cb
catherine <btstwice4ever@gmail.com>
2025-12-18 20:00:53 +0300

    password is working

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 $pass = 'jikook13';
-$hash = '$2y$12$vt/7DndlY14u3HKuW5St9uyuSP0JSWfhLhy9iA.FK7Z23KXC1mdPa';
+$hash = '$2y$12$.W9d46fAf0SKE5YVYW.sfeQ/N.MDxHGjUtRsjG8IYyBbgsCkVAi4.';
 if (password_verify($pass, $hash)) {
     echo "пароль верен!";
 } else {
